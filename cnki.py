361b10bbf1adb232d81c85a996737ab5bb7a9b19
Wang Yihang <wangyihanger@gmail.com>
2020-03-14 12:53:33 +0800

    [+] Selection for cnki search result

--- cnki.py
+++ cnki.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from __future__ import print_function, unicode_literals
+from whaaaaat import prompt, print_json
 import requests
 import bs4
 import os
@@ -46,17 +48,77 @@ def search(keyword):
             "authors": authors,
             "journal": journal,
             "href": href,
+            "docid": href.split("FileName=")[1].split("&DbName=")[0]
         })
     return result
 
-def select():
-    pass
+
+class Choice:
+    def __init__(self, title, authors, journal, docid):
+        self.title = title
+        self.authors = authors
+        self.journal = journal
+        self.docid = docid
+
+    def __str__(self):
+        return '[{}] {}. {}. {}'.format(
+            self.docid,
+            ",".join(self.authors), 
+            self.title,
+            self.journal,
+        )
+
+def select(choices):
+    cache = {}
+    c = []
+    for i in choices:
+        x = Choice(
+            authors=i['authors'],
+            journal=i['journal'],
+            title=i['title'],
+            docid=i['docid'],
+        )
+        choice = {"name": "{}".format(x)}
+        cache[i['docid']] = x
+        c.append(choice)
+    questions = [
+        {
+            'type': 'checkbox',
+            'name': 'documents',
+            'choices': c,
+            'message': 'Please select documents you want to export',
+        }
+    ]
+    answers = prompt(questions)
+    result = []
+    for i in answers["documents"]:
+        result.append(cache[i.split("[")[1].split("]")[0]])
+    return result
 
 def export():
     pass
 
 def main():
-    print(search("PHP代码缺陷检测技术的研究与实现"))
+    search_result = search("PHP代码缺陷检测技术的研究与实现")
+    selections = select(search_result)
+    print(selections)
+    # for choice in selections:
+    #     result_bib_filename = "{}{}{}.{}.bib".format(
+    #         folder,
+    #         os.path.sep,
+    #         choice.title,
+    #         ext,
+    #     )
+    #     title = choice.title
+    #     authors = ",".join(choice.authors)
+    #     docid = choice.docid
+    #     doctype = choice.doctype
+    #     data = export(docid, doctype)[0]
+    #     data["file"] = ":{}$\\backslash$:{}:{}".format(filename[0], filename[2:].replace(os.path.sep, "/"), ext)
+    #     bib = convert(data)
+    #     print(bib)
+    #     with open(result_bib_filename, "w", encoding="utf-8") as f:
+    #         f.write(bib)
 
 if __name__ == "__main__":
     main()
